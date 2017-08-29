2f9d6d87a8ca33052f2f77242c135ea6f6273db0
ILEANA <andreea.gorcsa@t-system.com>
2017-08-29 12:30:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/booklistingapp/Book.java
@@ -0,0 +1,49 @@
+package com.example.android.booklistingapp;
+
+/**
+ * Created by ILEANA on 8/23/2017.
+ */
+
+public class Book {
+
+    //The year of publication of the book
+    private int mYear;
+
+    //The author of the book
+    private String mAuthor;
+
+    // The location of the earthquake
+    private String mName;
+
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link Book} object.
+     *
+     * @param year   is the year of publication of the book
+     * @param author is the author who wrote the book
+     * @param name   is the name of the book
+     */
+    public Book(int year, String author, String name, String url) {
+        mYear = year;
+        mAuthor = author;
+        mName = name;
+        mUrl = url;
+    }
+
+    public int getYear() {
+        return mYear;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+}
---
+++ app/src/main/java/com/example/android/booklistingapp/BookAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.booklistingapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+/**
+ * Created by ILEANA on 8/23/2017.
+ */
+
+public class BookAdapter extends ArrayAdapter<Book> {
+
+    public BookAdapter(Context context, List<Book> books) {
+        super(context, 0, books);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.books_list_item, parent, false);
+
+        }
+
+        // Find the book at the given position in the list of books
+        Book currentBook = getItem(position);
+
+        //Find the TextView with the ID "bookYear"
+        TextView bookYear = (TextView) listItemView.findViewById(R.id.bookYear);
+        int intBookYear = currentBook.getYear();
+        String formattedBookYear = Integer.toString(intBookYear);
+        bookYear.setText(formattedBookYear);
+
+        //Find the TextView with the ID "bookAuthor"
+        TextView bookAuthor = (TextView) listItemView.findViewById(R.id.bookAuthor);
+        String formattedBookAuthor = currentBook.getAuthor();
+        bookAuthor.setText(formattedBookAuthor);
+
+        //Find the TextView with the IF "bookName"
+        TextView bookName = (TextView) listItemView.findViewById(R.id.bookName);
+        String formattedBookName = currentBook.getName();
+        bookName.setText(formattedBookName);
+
+        // Return the list item view that is now showing the appropriate data
+        return listItemView;
+    }
+}
